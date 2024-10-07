a9c487a7fe688ca8196eb67e2b9dea94dd5ff02d
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-07 13:08:49 -0300

    att

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMakTub',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,  # Necessário para incluir dados extras
+
+    package_data={
+        '': ['*.py'],
+        'src/bin': ['Maktub.so'],
+
+    },
+    install_requires=[],
+    author='Mateus',
+    author_email='mateusmoutinho01@gmail.com',
+    description='An Procedural Generation  Python Lib',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='seu git',  # URL do repositório
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@
 from src.PyMaktube.MakTub import MakTub
 
 m = MakTub("qdueaa porra é melhoroudd agorddda666")
-m.set_generation(0)
+m.set_generation(10000)
 
 for i in range(0,10):
     print(m.generate_token())
