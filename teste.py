b5a3ae8b653b9c31fddd2296297a94700ea75a52
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-07 13:28:40 -0300

    att

--- teste.py
+++ teste.py
@@ -1,9 +1,3 @@
 
 
-from src.PyMaktube.MakTub import MakTub
-
-m = MakTub("qdueaa porra é melhoroudd agorddda666")
-m.set_generation(10000)
-
-for i in range(0,10):
-    print(m.generate_token())
+from PyMaktube.MakTub import MakTub
